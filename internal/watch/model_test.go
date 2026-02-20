ddd7d225db965721a0f387d044d665ebb83951a7
Alex Hamlin <alex@alexhamlin.co>
2026-02-20 14:51:21 -0800

    Use testing.T.ArtifactDir with Spin
    This is much cleaner than the manual cleanup I did before, and provides
    the option to retain the generated files even on successful test runs.
    (On the other hand, test failures clean up the files rather than leaving
    them around for you to inspect, but Spin is consistent enough that a
    re-run should be fine.)

--- internal/watch/model_test.go
+++ internal/watch/model_test.go
@@ -26,13 +26,16 @@ var modelFile string
 //     processes that violates the model's assertions.
 //
 // This harness automates this process within the standard Go testing framework,
-// building and executing the model checker in a temporary directory and
-// displaying the details of any generated trail.
+// building the model checker into a test artifact directory and displaying the
+// details of any generated trail.
 //
 // Use the "modeltest" build tag to include this harness in a test run, and
-// include the "-v" flag to display the model checker's output. For example:
+// include the "-v" flag to display the model checker's output:
 //
-//	go test -v -tags modeltest -run Model ./internal/watch
+//	go test ./internal/watch -v -tags modeltest -run Model
+//
+// Include the "-artifacts" flag to persist the "pan" binary and any "*.trail"
+// file rather than let Go's test framework delete them automatically.
 //
 // [Spin]: https://spinroot.com/
 func TestModel(t *testing.T) {
@@ -42,24 +45,7 @@ func TestModel(t *testing.T) {
 		}
 	}
 
-	tmpdir, err := os.MkdirTemp("", "hypcast-spin-*")
-	if err != nil {
-		t.Fatalf("failed to create spin compilation directory: %v", err)
-	}
-
-	t.Logf("compiling model under %v", tmpdir)
-	t.Chdir(tmpdir)
-	t.Cleanup(func() {
-		if t.Failed() {
-			t.Logf("keeping %v due to test failure", tmpdir)
-			return
-		}
-		if err := os.RemoveAll(tmpdir); err == nil {
-			t.Logf("cleaned up %v", tmpdir)
-		} else {
-			t.Logf("failed to clean up %v: %v", tmpdir, err)
-		}
-	})
+	t.Chdir(t.ArtifactDir())
 
 	spin := exec.Command("spin", "-a", "/dev/stdin")
 	spin.Stdin = strings.NewReader(modelFile)
@@ -74,7 +60,7 @@ func TestModel(t *testing.T) {
 		t.Fatalf("failed to compile pan.c: %v", err)
 	}
 
-	pan := exec.Command(filepath.Join(tmpdir, "pan"))
+	pan := exec.Command(filepath.Join(t.ArtifactDir(), "pan"))
 	pan.Stdout, pan.Stderr = os.Stdout, os.Stderr
 	if err := pan.Run(); err != nil {
 		t.Fatalf("failed to run pan: %v", err)
